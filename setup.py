8db25742a97be9bda9d141957299452dc5f0d7ad
c0w <c0wtschpotato@gmail.com>
2019-10-27 11:45:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 #requirements for broadlink python from https://github.com/mjg59/python-broadlink, git clone git://github.com/xkonni/raspberry-remote.git ##forked to own git repo , make send and go
-#sudo apt-get install libzbar-dev libzbar0 libffi-dev libssl-dev raspberry-remote wiringpi
+#sudo apt-get install libzbar-dev libzbar0 libffi-dev libssl-dev raspberry-remote wiringpi libffi-dev
 #pip configparser
 
 
 import os
-from termcolor import colored
 
 print ("installing depencies")
 os.system("sudo apt-get install install libzbar-dev libzbar0 libffi-dev libssl-dev wiringpi")
-
+os.system("sudo pip install hermes")
+os.system("sudo pip install broadlink")
 print ("installing configparser & adafruit ws2801 driver")
 os.system("sudo pip install configparser")
-o.system("sudo pip install Adafruit_WS2801")
+os.system("sudo pip install Adafruit_WS2801")
 print ("installing git and python broadlink")
 os.system("sudo git clone https://github.com/mjg59/python-broadlink")
 r  = os.system("sudo chmod +x /home/pi/python-broadlink/setup.py")
