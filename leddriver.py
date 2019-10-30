1f57656ca8cda782e1796c5171873f5a2cf97a7c
c0w <c0wtschpotato@gmail.com>
2019-10-30 19:01:26 +0100

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -2,20 +2,23 @@ import paho.mqtt.client as mqtt
 
 HOST = '192.168.1.103'
 PORT = 1883
+current_status =""## init empty
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("HomA/ledstrip1")
+    client.subscribe("HomA/ledstrip1/set_status")
 
 def on_message(client, userdata, msg):
 	print(msg)
 	print(msg.payload)
-	if msg.topic == "HomA/ledstri1":
-		print("ledstrip 1 angesprochen")
-		if msg.payload == "status":
-			print("returning status i last got")
-		else:
-			current_status = msg.payload
-			print("set status to: %s", msg.payload)
+	if msg.topic == "HomA/ledstrip1/set_status":
+		print("ledstrip 1 status set: "+msg.payload)
+		
+		
+	if msg.topic =="HomA/ledstrip1/get_status":
+		client.publish(msg.topic,current_status)
+		print("published status "+current_status)
+
 		
 
 
