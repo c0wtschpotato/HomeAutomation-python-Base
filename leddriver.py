c7e0645324b80f1f276debf080ca99f4e02698de
c0w <c0wtschpotato@gmail.com>
2019-11-01 11:19:14 +0100

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -44,7 +44,7 @@ def set_leds_to_input(sentpayload):
 		# print(typeof(obj["number_of_running"]))
 		# ws.running_on_chain(ws.pixels,(int(obj["basecolor"]["r"]),int(obj["basecolor"]["g"]),int(obj["basecolor"]["b"])),(int(obj["runningcolor"]["r"]),int(obj["runningcolor"]["g"]),int(obj["runningcolor"]["b"])),int(obj["number_of_running"]),float(obj["sleep_time"]))
 		print("running on chain with hardcoded inputs")
-		ws.running_on_chain(ws.pixels,(255,0,0),(0,255,0),int(obj["number_of_running"]),float(obj["sleep_time"]))
+		ws.running_on_chain(ws.pixels,(255,0,0),(int(obj["runningcolor"]["r"]),int(obj["runningcolor"]["g"]),int(obj["runningcolor"]["b"])),int(obj["number_of_running"]),float(obj["sleep_time"]))
 
 
 client = mqtt.Client()
