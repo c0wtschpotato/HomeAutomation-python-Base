ff7c750ef3edd1051c8376c4b2454c28f1149d28
c0w <c0wtschpotato@gmail.com>
2019-11-01 23:35:45 +0100

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -19,6 +19,7 @@ def loopfunc(input_payload):###function used with threading to loop certain effe
 	        if do_run == False:
 				if last_status !="free":
 					print("hitting out laststatus")
+					print(last_status)
 					set_leds_to_input()
 					return
 				elif last_status == None:
