27c5f7991c2a08c5174d323a93f1c1f834fcfd87
c0w <c0wtschpotato@gmail.com>
2019-11-08 01:48:23 +0100

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -91,6 +91,8 @@ def set_leds_to_input(sentpayload):
 
 	if obj["function"] == "setalltocolor":
 		ws.setalltocolor(ws.pixels,(int(obj["basecolor"]["r"]),int(obj["basecolor"]["g"]),int(obj["basecolor"]["b"])))
+	if obj["function"] == "rainbow_colors":
+		ws.rainbow_colors(ws.pixels,0.01)
 
 client = mqtt.Client()
 client.on_connect = on_connect
