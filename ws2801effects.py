00cdefe53ace318c6e6c19f45e8746aaf803f306
c0w <c0wtschpotato@gmail.com>
2019-11-09 15:43:04 +0100

    Update ws2801effects.py

--- ws2801effects.py
+++ ws2801effects.py
@@ -149,23 +149,22 @@ def lightning(pixels):
 def burning(pixels,basecolor=(120,30,0), runningcolor= (255, 10, 0), number_of_running= 15):
 
     setalltocolor(pixels,(int(basecolor[0]),int(basecolor[1]),int(basecolor[2])))
-    while 1:
-        startpixel = randrange(0,PIXEL_COUNT-number_of_running)
-        for i in range(startpixel,startpixel+number_of_running):###maximum all pixels - number of changed
-            pixels.set_pixel(i,Adafruit_WS2801.RGB_to_color(runningcolor[0],runningcolor[1],runningcolor[2]))
-        pixels.show()
-        for j in range(0,30):
-            brightness(pixels,(i,i + number_of_running),1,"decrease")
-            time.sleep(0.02)
-        for j in range(0,30):
-            brightness(pixels,(i,i + number_of_running),1,"increase")
-            time.sleep(0.02)
-        for j in range(0,30):
-            brightness(pixels,(0,PIXEL_COUNT),1,"decrease")
-            time.sleep(0.01)
-        for j in range(0,30):
-            brightness(pixels,(0,PIXEL_COUNT),1,"increase")
-            time.sleep(0.01)
+    startpixel = randrange(0,PIXEL_COUNT-number_of_running)
+    for i in range(startpixel,startpixel+number_of_running):###maximum all pixels - number of changed
+        pixels.set_pixel(i,Adafruit_WS2801.RGB_to_color(runningcolor[0],runningcolor[1],runningcolor[2]))
+    pixels.show()
+    for j in range(0,30):
+        brightness(pixels,(i,i + number_of_running),1,"decrease")
+        time.sleep(0.02)
+    for j in range(0,30):
+        brightness(pixels,(i,i + number_of_running),1,"increase")
+        time.sleep(0.02)
+    for j in range(0,30):
+        brightness(pixels,(0,PIXEL_COUNT),1,"decrease")
+        time.sleep(0.01)
+    for j in range(0,30):
+        brightness(pixels,(0,PIXEL_COUNT),1,"increase")
+        time.sleep(0.01)
 
 
 def setalltocolor(pixels,color=(255,255,255)):
