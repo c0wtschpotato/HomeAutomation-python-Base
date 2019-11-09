ab2c40e85164ee354f927336bac480c8fff554dc
c0w <c0wtschpotato@gmail.com>
2019-11-09 16:23:42 +0100

    Update ws2801effects.py

--- ws2801effects.py
+++ ws2801effects.py
@@ -163,22 +163,22 @@ def burning(pixels,basecolor=(120,30,0), runningcolor= (255, 10, 0), number_of_r
 
     for j in range(0,30):
         brightness(pixels,(startpixel,startpixel + number_of_running),1,"decrease",(None,"g","b"))
-        time.sleep(0.1)
-        print(j)
+        time.sleep(0.1)        
     print("decreased spot")
-    for j in range(0,30):
-        brightness(pixels,(startpixel,startpixel + number_of_running),1,"increase",(None,"b","b"))
-        print(j)
+
+    for j in range(0,50):
+        brightness(pixels,(startpixel,startpixel + number_of_running),1,"increase",(None,"g","b"))
         time.sleep(0.1)
     print("increased spot")
+
     for j in range(0,60):
         brightness(pixels,(0,PIXEL_COUNT),1,"decrease",(None,"g","b"))
-        print(j)
         time.sleep(0.05)
-    print("decreased all")
+        print("decreased all")
+
     for j in range(0,60):
         brightness(pixels,(0,PIXEL_COUNT),1,"increase",(None,"g","b"))
-        print(j)
+        
         time.sleep(0.05)
     print("increased all")
 
