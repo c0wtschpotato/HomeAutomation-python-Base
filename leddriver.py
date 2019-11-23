e395fba42df23adc3628832c93a45bc10b86ca67
c0w <c0wtschpotato@gmail.com>
2019-11-23 17:35:37 +0100

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -27,15 +27,15 @@ def loopfunc(input_payload,pickfunc):###function used with threading to loop cer
 			ws.running_on_chain(ws.pixels,(r,g,b),(255 -r ,255-g,255-b),5,0.01)
 		if pickfunc == 0:
 			ws.rainbow_cycle(ws.pixels,0.0035)
-		        if do_run == False:
-					if last_status !="free":
-						print("setting back leds")
-						print(last_status)
-						# time.sleep(1)### wait a second to reset status
-						set_leds_to_input(last_status)
-						return
-					elif last_status == None:
-						return
+		if do_run == False:
+			if last_status !="free":
+				print("setting back leds")
+				print(last_status)
+				# time.sleep(1)### wait a second to reset status
+				set_leds_to_input(last_status)
+				return
+			elif last_status == None:
+				return
 	return
 
 def on_connect(client, userdata, flags, rc):
