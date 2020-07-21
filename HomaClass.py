1364dbbbf9cdb6dd9835144164b014667277ca06
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-21 20:23:17 +0200

    Update HomaClass.py
    self channel name set as string

--- HomaClass.py
+++ HomaClass.py
@@ -74,8 +74,8 @@ class HTS():
 				j = i
 				print(" reached Channel 6 so its 0")
 			if self.channel == int(targetchannel):
-				self.channelname == self.channelnameS[int(self.channel)]
-				print("Selfchannel is targetchannel  "+ self.channelnameS[int(self.channel)])
+				self.channelname == str(self.channelnameS[int(self.channel)])
+				print("Selfchannel is targetchannel "+ self.channelnameS[int(self.channel)])
 				break
 			time.sleep(1)
 		
