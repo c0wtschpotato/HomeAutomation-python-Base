8e4669cc2242c2e6b4c6a62ccc8cec426781b73e
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-21 19:01:58 +0200

    Update HomaClass.py

--- HomaClass.py
+++ HomaClass.py
@@ -47,12 +47,12 @@ class HTS():
 		os.system(self.sendPath+"@/home/pi/python-broadlink/cli/philips-down.vol_change")
 
 	def channel(self,targetchannel):#0 hdmi, 1 fm, 2 bt, 3 apps, 4bd in, 5 aux
-		i = abs(int(self.channel) - int(targetchannel))
-		for j in range(0,i):
-			os.system(rmSendPath+"@/home/pi/python-broadlink/cli/philips-switch.source")
-			time.sleep(0.2)
+		# i = abs(int(self.channel) - int(targetchannel))
+		# for j in range(0,i):
+			# os.system(rmSendPath+"@/home/pi/python-broadlink/cli/philips-switch.source")
+			# time.sleep(0.2)
 		# print("switching " + str(i) +" times")
-
+		print("switching ")
 hts = HTS()
 
 
