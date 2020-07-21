97eda8682a20462472d170451f64fafd49fc6779
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-21 18:15:59 +0200

    Update HomaClass.py

--- HomaClass.py
+++ HomaClass.py
@@ -5,28 +5,40 @@ import paho.mqtt.client as mqtt
 import os
 import configparser, time, json
 
-class MHZ():
+class MHZ():##Class for all MHZ controlled parts
  	"""docstring for MHZ"""
- 	def __init__(self, address, units,status):
+ 	def __init__(self, address, units,status,name):
  		self.address = address
  		self.units = units
  		self.status = status
+ 		self.name = name
  	def set(self,status):
+ 		if status == "on" or status == "an":
+ 			status = "1"
+ 		if status == "off" or status == "aus":
+ 			status = "0"
  		os.system("sudo /home/pi/raspberry-remote/./send "+self.address+" " +self.units + " "+str(status))
  		self.status = status
+ 	def on(self):
+ 		os.system("sudo /home/pi/raspberry-remote/./send "+self.address+" " +self.units + " 1")
+ 	def off(self):
+ 		os.system("sudo /home/pi/raspberry-remote/./send "+self.address+" " +self.units + " 0")
 
 
+mhz1 = MHZ("11001","1","0","philips")
+mhz2 = MHZ("11001","2","0", "screen")
+mhz3 = MHZ("11001","3","0", "led2")
+mhz4 = MHZ("11001","4","0", "unused")
+mhz5 = MHZ("11001","5","0","unused")
 
-mhz1 = MHZ("11001","1","0")
-mhz2 = MHZ("11001","2","0")
-mhz3 = MHZ("11001","3","0")
-mhz4 = MHZ("11001","4","0")
-mhz5 = MHZ("11001","5","0")
-
-
-class ClassName(object):
+class HTS():
 	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
+	rmSendPath = "sudo python /home/pi/python-broadlink/cli/./broadlink_cli --type 0x2737 --host 192.168.1.104 --mac 65c55834ea34 --send "
+	
+	os.system(rmSendPath+"@/home/pi/python-broadlink/cli/philips-up.vol_change")
+
+	def __init__(self, name):
+		self.name = "Samsung Home Theater"
+
+		
 		
