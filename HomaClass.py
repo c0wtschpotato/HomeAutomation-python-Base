05fd2c4a80a1c87e465c067fd1419bedbbdc09b6
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-22 16:57:58 +0200

    Update HomaClass.py
    volume logging, class for all mhz controlls.

--- HomaClass.py
+++ HomaClass.py
@@ -31,6 +31,12 @@ mhz3 = MHZ("11001","3","0", "led2")
 mhz4 = MHZ("11001","4","0", "unused")
 mhz5 = MHZ("11001","5","0","unused")
 
+
+class mhzgrp():
+	def __init__(self)
+	self.all = [mhz1,mhz2,mhz3,mhz4,mhz5]
+	
+
 class HTS():
 	"""docstring for ClassName"""
 	# rmSendPath = "sudo python /home/pi/python-broadlink/cli/./broadlink_cli --type 0x2737 --host 192.168.1.104 --mac 65c55834ea34 --send "
@@ -42,12 +48,16 @@ class HTS():
 		self.channels = {"HDMI":0,"FM":1,"BT":2,"APPS":3,"BD":4,"AUX":5}
 		self.channelnameS = ["HDMI", "FM", "BT","APPS","BD","AUX"]
 		self.sendPath = "sudo python /home/pi/python-broadlink/cli/./broadlink_cli --type 0x2737 --host 192.168.1.104 --mac 65c55834ea34 --send "
+		self.volume = 15
+
 
 	def volup(self):
 		os.system(self.sendPath+"@/home/pi/python-broadlink/cli/philips-up.vol_change")
+		self.volume = self.volume +1
 
 	def voldown(self):
 		os.system(self.sendPath+"@/home/pi/python-broadlink/cli/philips-down.vol_change")
+		self.volume = self.volume -1
 
 	def targetchannel(self,targetchannel):#0 hdmi, 1 fm, 2 bt, 3 apps, 4bd in, 5 aux
 		if targetchannel == "HDMI":
@@ -63,17 +73,17 @@ class HTS():
 		if targetchannel == "AUX":
 			targetchannel = self.channels["AUX"]
 
-		i = 5
+		i = 5## max channels to iterate through
 		print("switching " + str(i) +" times")
 		for j in range(0,i):
 			self.channel = int(self.channel)+1
 			print("switched "+ str(j+1)+ " times. Now on " + str(self.channel))
 			os.system(self.sendPath+"@/home/pi/python-broadlink/cli/philips.switch")
-			if self.channel >= 6:
+			if self.channel >= i+1:#max channels or bigger becomes 0
 				self.channel = 0
 				j = i
 				print(" reached Channel 6 so its 0")
-			if self.channel == int(targetchannel):
+			if self.channel == int(targetchannel):#correct channel, set attributes and break
 				print("channelname was "+str(self.channelname))
 				self.channelname = str(self.channelnameS[self.channel])
 
