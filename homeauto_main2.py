db4a683a4801c0a610af6fe0c5acca7d6ebca12c
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-22 19:40:05 +0200

    Update homeauto_main2.py

--- homeauto_main2.py
+++ homeauto_main2.py
@@ -134,7 +134,7 @@ def on_message(client, userdata, msg):
 
         if msg.topic == "HomA/hts":
                 print("Message on HTS:")
-                print(json.loads(msg.payload))
+                # print(json.loads(msg.payload))
                 erhalten = json.loads(msg.payload, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
                 print(erhalten)
 
