b2cb27a6bae5336de6c34fc6df7efedf5e3b1c1c
c0wtschpotato <c0wtschpotato@gmail.com>
2020-07-23 19:05:27 +0200

    Update homeauto_main2.py

--- homeauto_main2.py
+++ homeauto_main2.py
@@ -129,7 +129,7 @@ def msgToClass(msg):
 
 def on_message(client, userdata, msg):
         print(msg.payload)
-        # rec = Recieved(msg.payload)
+        rec = msg.payload
         if msg.topic == "HomA/status":
                 print("Status request for "+str(msg.payload))
                 if msg.payload == "hts":
@@ -139,6 +139,7 @@ def on_message(client, userdata, msg):
 
         if msg.topic == "HomA/hts/cmd":#### needs more than one parameter, send JSON {"CMD":"value"} and change class to get {"VOLUP":"byAmount"}
                 print("Command on HTS:")
+                print(msg)
                 print("got rec "+str(rec[0]))
 
                 if rec[0] == "volup":
