5942b22fa1a438d50c5adb0046e9bfa82f1ceec2
c0w <c0wtschpotato@gmail.com>
2020-08-13 13:17:15 +0200

    Update leddriver.py

--- leddriver.py
+++ leddriver.py
@@ -134,6 +134,7 @@ def set_leds_to_input(sentpayload):
 	if obj["function"] == "percentOfAll":
 		print("in func percentOfAll")
 		print(obj["percent"])
+		print(int(obj["coloron"][0]),int(obj["coloron"][1]),int(obj["coloron"][2]))
 		ws.percentOfAll(ws.pixels,int(obj["percent"]),(int(obj["coloron"][0]),int(obj["coloron"][1]),int(obj["coloron"][2])),(int(obj["colorOff"][0]),int(obj["colorOff"][0]),int(obj["colorOff"][2])))
 
 	if obj["function"] == "rainbow_colors":
