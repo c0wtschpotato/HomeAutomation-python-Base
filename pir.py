8dc2e777b61efdf35b789299a309306817a76178
c0w <c0wtschpotato@gmail.com>
2025-03-08 22:43:02 +0100

    Update pir.py

--- pir.py
+++ pir.py
@@ -43,6 +43,7 @@ def no_motion_function():
         delay += 1
         print(f'delayed {delay}')
     if delay > 5:
+        print(delay)
         client.publish("HomA/move1",0)
         client.publish("gv2mqtt/light/3ACA983DAE115A38/command",'{"state":"OFF"}')
         client.publish("gv2mqtt/light/D67AB08184CE6070/command",'{"state":"OFF"}')
