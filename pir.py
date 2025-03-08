f88666b666154224c11b7d944dc68dcce6e49b34
c0w <c0wtschpotato@gmail.com>
2025-03-08 21:40:10 +0100

    added check for night

--- pir.py
+++ pir.py
@@ -4,10 +4,12 @@ from gpiozero import MotionSensor
 from signal import pause
 import paho.mqtt.client as mqtt
 import time 
+import datetime, astral
+
 
 HOST = "192.168.1.107"
 pir = MotionSensor(18)
-
+observer = astral.observer(longitude = 48.572195884199324, latitude = 13.43809806362507, elevation = 312)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -22,18 +24,27 @@ def on_connect(client, userdata, flags, rc):
 
 def motion_function():
     client.publish("HomA/move1",1)
-    client.publish("gv2mqtt/light/3ACA983DAE115A38/command",'{"state":"ON"}')
-    client.publish("gv2mqtt/light/D67AB08184CE6070/command",'{"state":"ON"}')
-    print("published movement")
+    if hotornot() is True:
+        client.publish("gv2mqtt/light/3ACA983DAE115A38/command",'{"state":"ON"}')
+        client.publish("gv2mqtt/light/D67AB08184CE6070/command",'{"state":"ON"}')
+        print("published lights on")
+    else:
+        print("Movement but not night")
 
 def no_motion_function():
     client.publish("HomA/move1",0)
     client.publish("gv2mqtt/light/3ACA983DAE115A38/command",'{"state":"OFF"}')
     client.publish("gv2mqtt/light/D67AB08184CE6070/command",'{"state":"OFF"}')
     print("published movement stopped")
+
+def hotornot():
+    if datetime.datetime.utcnow() > astral.sun.night(obs)[1].replace(tzinfo=None) is True and datetime.datetime.utcnow() < astral.sun.night(obs)[0].replace(tzinfo=None) is True:
+        print("Es ist Nacht")
+        return True
+
+
 client = mqtt.Client()
 client.on_connect = on_connect
-
 
 pir.when_motion = motion_function
 pir.when_no_motion = no_motion_function
