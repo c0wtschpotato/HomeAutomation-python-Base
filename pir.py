10a8abfab4a602b86e89a53302bef8c967cab2f7
c0w <c0wtschpotato@gmail.com>
2025-03-08 21:58:51 +0100

    Update pir.py

--- pir.py
+++ pir.py
@@ -38,11 +38,11 @@ def no_motion_function():
     print("published movement stopped")
 
 def hotornot():
-    if datetime.datetime.utcnow() > astral.sun.night(observer)[1].replace(tzinfo=None) is True:
+    if datetime.datetime.utcnow() < astral.sun.night(observer)[1].replace(tzinfo=None) is True:
         print("es ist vor Sonnenaufgang")
-    if datetime.datetime.utcnow() < astral.sun.night(observer)[0].replace(tzinfo=None) is True:
+    if datetime.datetime.utcnow() > astral.sun.night(observer)[0].replace(tzinfo=None) is True:
         print("es ist nach Sonnenuntergang")
-    if datetime.datetime.utcnow() > astral.sun.night(observer)[1].replace(tzinfo=None) is True and datetime.datetime.utcnow() < astral.sun.night(observer)[0].replace(tzinfo=None) is True:
+    if datetime.datetime.utcnow() < astral.sun.night(observer)[1].replace(tzinfo=None) is True and datetime.datetime.utcnow() > astral.sun.night(observer)[0].replace(tzinfo=None) is True:
         print("Es ist Nacht")
         return True
 
