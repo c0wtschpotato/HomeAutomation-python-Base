6744f2ba0523c10e732a111f51a09578350e1fef
c0w <c0wtschpotato@gmail.com>
2025-03-08 18:13:57 +0100

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,19 +1,39 @@
-import RPi.GPIO as GPIO
-import time
- 
-SENSOR_PIN = 23
- 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SENSOR_PIN, GPIO.IN)
- 
-def mein_callback(channel):
-    # Hier kann alternativ eine Anwendung/Befehl etc. gestartet werden.
-    print('Es gab eine Bewegung!')
- 
-try:
-    GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=mein_callback)
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Beende...")
-GPIO.cleanup()
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from gpiozero import MotionSensor
+from signal import pause
+import paho.mqtt.client as mqtt
+
+HOST = "192.168.1.107"
+pir = MotionSensor(18)
+client.on_connect = on_connect
+client.connect(HOST, 1883, 60)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("HomA/ledstrip1/get_status")## for internal comm
+    client.subscribe("HomA/ledstrip1/set_status")
+    client.subscribe("hermes/hotword/default/detected")##gets hermes action from snips and starts led strip
+    client.subscribe("hermes/hotword/toggleOn")
+    client.subscribe("c0wtschpotato:PCcontrol")
+    client.subscribe("HomA/Philips/set_status")
+    client.subscribe("HomA/kitchen/move")
+
+def motion_function():
+    client.publish("HomA/move1",1)
+
+def no_motion_function():
+    client.publish("HomA/move1",0)
+
+pir.when_motion = motion_function
+pir.when_no_motion = no_motion_function
+client.on_message = on_message
+client.connect(HOST, PORT, 60)
+client.loop_forever()
+
+
+pause()
+
+
