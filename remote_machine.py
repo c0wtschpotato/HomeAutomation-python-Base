597847c22dd3218a5ad1e82945bce24476f8ddf1
c0w <c0wtschpotato@gmail.com>
2025-05-04 13:26:00 +0200

    Update remote_machine.py

--- remote_machine.py
+++ remote_machine.py
@@ -1,24 +1,47 @@
 
-import os
+import os,json
 try:
 	import paho.mqtt.client as mqtt
 except:
 	os.system("pip install paho-mqtt")
+
+
 ####################################################
 ###########Commands#################################
 #shutdown - shutdown machine#
 
 
 
+config_file = 'config.json'
 
-HOST = input("Are you in Host network? y/n:")
-if HOST == "y":
-	HOST = "192.168.1.107"
-else:
-	HOST = "192.168.0.51"
 PORT = 1883
-global channel
+global customchannel
 customchannel = input("Name the mqtt channel to listen to (HomA/remote/CHANNELNAME)")
+
+def create_or_read_config():
+    if os.path.exists(config_file):
+        with open(config_file, 'r') as file:
+            config = json.load(file)
+            print("Konfiguration geladen:")
+            print(f"Host: {config['host']}")
+            print(f"Custom Channel: {config['customchannel']}")
+    else:
+        host = input("Are you in Host network? y/n:")
+			if host == "y":
+				host = "192.168.1.107"
+			else:
+				host = "192.168.0.51"
+        customchannel = input("Bitte geben Sie den Custom Channel ein: ")
+
+        config = {
+            'host': host,
+            'customchannel': customchannel
+        }
+
+        with open(config_file, 'w') as file:
+            json.dump(config, file, indent=4)
+
+        print("Konfiguration gespeichert.")
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -40,6 +63,6 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(HOST, 1883, 60)
+client.connect(host, 1883, 60)
 
 client.loop_forever()
